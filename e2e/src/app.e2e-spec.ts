b5cf13f691f5c6ac1763a7de774be16a5ef48bde
Joseph Lin <joseph@xtremeapp.com.tw>
2021-09-22 00:48:20 +0800

    調整專案名稱

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('route-sample app is running!');
+    expect(await page.getTitleText()).toEqual('ng-seo-sample app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/route-sample'),
+      dir: require('path').join(__dirname, './coverage/ng-seo-sample'),
       subdir: '.',
       reporters: [
         { type: 'html' },
